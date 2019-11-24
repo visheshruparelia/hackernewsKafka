31c70c4aaec3b3804a0722c1a605c5663a935805
tanubordia <tb24697@gmail.com>
2019-11-24 15:01:21 +0530

    Add files via upload

--- consumerflask.py
+++ consumerflask.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from kafka import KafkaConsumer
 # from pymongo import MongoClient
 from json import loads
@@ -6,7 +6,7 @@ import pandas as pd
 
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route("/")
 def hello():
@@ -18,11 +18,16 @@ def hello():
          group_id='HNgroup',
          value_deserializer=lambda x: loads(x.decode('utf-8')))
     list=[]
+    i=0
 
     for message in consumer:
         message = message.value
+        i=i+1
         print ('{} added.'.format(message))
         list.append(message)
+        if(i==10):
+            break
+    print(i)
 
     return render_template('index.html', list=list)
 
